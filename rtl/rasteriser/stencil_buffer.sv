a7d33fededa2eedae3806d04c22ea28bfb0f49c0
booth-algo <kvl01@protonmail.com>
2024-09-05 22:41:32 +0100

    z-buffer added

--- rtl/rasteriser/stencil_buffer.sv
+++ rtl/rasteriser/stencil_buffer.sv
@@ -1,9 +1,33 @@
 module stencil_buffer #(
     parameter WIDTH = 32;
 )(
+    input logic clk_i,
 
+    // GPU control signals
+    input logic [7:0] ref_val_i, // stencil reference value
+    input logic start_i, // start stencil test
+
+    // Fragment shader output
+    
+
+    // mask input
+    // test function input
+    // stencil buffer value
+
+    input logic [WIDTH-1:0] stencil_i, // Stencil value from fragment shader
+
+    output logic stencil_pass_o
 
 );
+    
+    /* 
+        The stencil buffer typically shares the same memory space as the z-buffer
+        
+        Alpha test -> stencil test -> depth test
+
+        The stencil buffer is used to mask out fragments that should not be rendered
+        The fragment's stencil value (unsigned integer) is tested against the stencil buffer's value - if the test fails, the fragment is culled
+    */
 
 
 endmodule
--- rtl/rasteriser/z_buffer.sv
+++ rtl/rasteriser/z_buffer.sv
@@ -1,19 +1,55 @@
 module z_buffer #(
-    parameter WIDTH = 32,
+    parameter PIXEL_SIZE = 8,
     parameter X_RES = 1280,
     parameter Y_RES = 720
 )(
-    input wire clk_i,
+    input logic clk_i,
+    input logic start_i,
+    input logic flush_i, // reset buffer every frame
+    input logic pixel_x_i, pixel_y_i, pixel_z_i,
 
-    output reg idk_o
+    output logic render_o,
+    output logic done_o
 );
 
     /*
-        The z-depth test buffer should provide
-            1. Pixel coordinates
-            2. Barycentric coordinates
+        Tests if the fragment should be rendered by comparing the fragment's z value to the z-buffer's value
 
-        which allows the SEU shader to interpolate texture coordinates from barycentric coordinates
+        ! Implementation of the z-buffer can be modified based on whether you are normalizing the depth values to [0,1]
     */
+
+    // Single pixel buffer
+    typedef struct packed {
+        logic valid;
+        logic [PIXEL_SIZE-1:0] z;
+    } pixel_buffer;
+
+    // Z-buffer memory
+    pixel_buffer z_buffer[X_RES-1:0][Y_RES-1:0] = '{default: '{valid: 1'b0, z: '0}};
+
+    always_ff @(posedge clk_i) begin
+        if (start_i) begin
+            
+            if (!z_buffer[pixel_x_i][pixel_y_i].valid) begin
+                // If value is not valid, write to buffer
+                z_buffer[pixel_x_i][pixel_y_i].z <= pixel_z_i;
+                z_buffer[pixel_x_i][pixel_y_i].valid <= 1;
+                render_o <= 1;
+            end
+            else if (pixel_z_i < z_buffer[pixel_x_i][pixel_y_i].z) begin
+                z_buffer[pixel_x_i][pixel_y_i].z <= pixel_z_i;
+                render_o <= 1;
+            end
+            else begin
+                render_o <= 0;
+            end
+
+        end else if (flush_i) begin
+            z_buffer[pixel_x_i][pixel_y_i].valid <= 0;
+        end
+        else begin
+            done_o <= 0;
+        end
+    end
 
 endmodule
