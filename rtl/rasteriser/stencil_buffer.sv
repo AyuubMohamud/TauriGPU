325dd9f10b906961e3754c241c5fd4cc0dd45ec4
booth-algo <kvl01@protonmail.com>
2024-09-16 17:19:23 +0100

    Modified z-buffer, writing stencil buffer

--- rtl/rasteriser/stencil_buffer.sv
+++ rtl/rasteriser/stencil_buffer.sv
@@ -1,20 +1,26 @@
 module stencil_buffer #(
     parameter PIXEL_SIZE = 32,
-    parameter BUFFER_SIZE = 8,
+    parameter STENCIL_SIZE = 8,
     parameter X_RES = 1280,
     parameter Y_RES = 720
 )(
     input logic clk_i,
 
-    // GPU control signals
-    input logic [7:0] ref_val_i, // stencil reference value
-    input logic start_i, // start stencil test
-    input logic [2:0] func_i, // stencil test function
-    input logic [?] mask_i, // !stencil mask
-    input logic [PIXEL_SIZE-1:0] stencil_i, // Stencil value from fragment shader
+    // Stencil func control signals
+    input logic start_i,                                            // start stencil test (acts as "valid")
+    input logic [STENCIL_SIZE-1:0] ref_val_i,                       // stencil reference value
+    input logic [2:0] stencil_func_i,                               // stencil test function
+    input logic [PIXEL_SIZE-1:0] mask_i[X_RES-1:0][Y_RES-1:0],      // stencil mask
 
-    output logic stencil_pass_o,
+    input logic [STENCIL_SIZE-1:0] frag_stencil_i,                  // stencil value from fragment shader
+
+    // Stencil op control signals
+    input logic [2:0] stencil_op_i,                                 // stencil operation
+    input logic depth_pass_i,                                       // depth test result (from z_buffer's depth_pass_o)
+
+    output logic stencil_pass_o,                                    // stencil test result
     output logic done_o
+
 );
     
     /* 
@@ -27,7 +33,21 @@ module stencil_buffer #(
     */
 
     // Stencil buffer
-    logic [PIXEL_SIZE-1:0] stencil_buffer [X_RES-1:0][Y_RES-1:0] = '{default: '0};
+    typedef struct packed {
+        logic valid;
+        logic [STENCIL_SIZE-1:0] stencil;
+    } stencil_buffer;
+
+    stencil_buffer stencil_buffer_array[X_RES-1:0][Y_RES-1:0];
+
+    // Init buffer
+    initial begin
+        for (int x = 0; x < X_RES; x++) begin
+            for (int y = 0; y < Y_RES; y++) begin
+                stencil_buffer_array[x][y] = 0;
+            end
+        end
+    end
 
     typedef enum logic [2:0] {
         GL_NEVER = 3'b000,
@@ -40,23 +60,56 @@ module stencil_buffer #(
         GL_ALWAYS = 3'b111
     } stencil_func_t;
 
-    always_ff @(posedge clk) begin
-        if (start_i) begin
+    // State machine
+    typedef enum logic [1:0] {
+        IDLE,
+        STENCIL_TEST,
+        DONE
+    } state_t;
+
+    state_t curr_state, next_state;
+
+    always_comb begin
+        next_state = curr_state;
+        case (curr_state)
+            IDLE: next_state = STENCIL_TEST;
+            STENCIL_TEST: next_state = DONE;
+            DONE: next_state = IDLE;
+        endcase
+    end
+
+    always_ff @(posedge clk_i) begin
+        // State machine
+        if (!start_i) begin
+            curr_state <= IDLE;
+        end else begin
+            curr_state <= next_state;
+        end
+
+        // Stencil test state
+
+
+        // Done state
+        if (curr_state == DONE) begin
+            done_o <= 1;
+        end else begin
             done_o <= 0;
         end
     end
 
     always_comb begin
-        case (func_i)
+        case (stencil_func_i)
             GL_NEVER: stencil_pass_o = 0;
-            GL_LESS: stencil_pass_o = (stencil_i < ref_val_i);
-            GL_LEQUAL: stencil_pass_o = (stencil_i <= ref_val_i);
-            GL_GREATER: stencil_pass_o = (stencil_i > ref_val_i);
-            GL_GEQUAL: stencil_pass_o = (stencil_i >= ref_val_i);
-            GL_EQUAL: stencil_pass_o = (stencil_i == ref_val_i);
-            GL_NOTEQUAL: stencil_pass_o = (stencil_i != ref_val_i);
+            GL_LESS: stencil_pass_o = (frag_stencil_i < ref_val_i);
+            GL_LEQUAL: stencil_pass_o = (frag_stencil_i <= ref_val_i);
+            GL_GREATER: stencil_pass_o = (frag_stencil_i > ref_val_i);
+            GL_GEQUAL: stencil_pass_o = (frag_stencil_i >= ref_val_i);
+            GL_EQUAL: stencil_pass_o = (frag_stencil_i == ref_val_i);
+            GL_NOTEQUAL: stencil_pass_o = (frag_stencil_i != ref_val_i);
             GL_ALWAYS: stencil_pass_o = 1;
-            default: stencil_pass_o = 0;
+
+            /* OpenGL: By default, the stencil function GL_ALWAYS is used*/
+            default: stencil_pass_o = 1;
         endcase
     end
 
